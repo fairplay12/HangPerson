268fe17da2e82938c3b8acfd2988f3c3bb753568
fairplay12 <vladimir.shvets1206@gmail.com>
2017-02-05 15:52:00 +0200

    initialize hangperson project

---
+++ src/main/java/com/hangperson/HangpersonApplication.java
@@ -0,0 +1,12 @@
+package com.hangperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangpersonApplication.class, args);
+	}
+}
