33ad6ec72b813061b6ccb3b3d709bcddcbf08861
fairplay12 <vladimir.shvets1206@gmail.com>
2017-02-11 16:26:18 +0200

    added random word feature

--- src/main/java/com/hangperson/controller/HangPersonController.java
+++ src/main/java/com/hangperson/controller/HangPersonController.java
@@ -2,7 +2,11 @@ package com.hangperson.controller;
 
 import com.hangperson.infrastructure.GameChecker;
 import com.hangperson.infrastructure.HangPersonGame;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by vova on 05.02.17.
@@ -30,9 +34,13 @@ public class HangPersonController {
 
     private String getRandomWord() throws Exception {
 
-        String url = "http://watchout4snakes.com/wo4snakes/Random/RandomWord";
+        //String link = "http://watchout4snakes.com/wo4snakes/Random/RandomWord";
+        String word;
+        String link = "http://www.setgetgo.com/randomword/get.php";
+        RestTemplate restTemplate = new RestTemplate();
+        word = restTemplate.getForObject(link, String.class);
+        return word.toLowerCase();
 
-        return "somestring";
     }
 
 
